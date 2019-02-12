cb04492ff866ece3cb3f729bae0e5717512afca8
meselfi <wmsopou@gmail.com>
2019-02-12 19:09:08 +0100

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mock Services with the Maven SOAPUI plugin
+
+A Maven project that shows how generate a JAX-WS service client and test it against a mock service using the SOAPUI Maven plugin.
+
+## Prerequisites
+
+Maven 3.5.0 installed and "mvn" on PATH.
+JDK 1.8 installed and JAVA_HOME pointing the the root folder of the JDK.
+
+## Build and run test
+
+>git clone https://github.com/SorenPoulsen/mock-services-with-maven-soapui-plugin.git
+>cd mock-services-with-maven-soapui-plugin/ServiceMock
+>mvn clean test
+
---
+++ ServiceMock/ServiceClient/src/test/java/com/sorenpoulsen/ws/ServiceTest.java
@@ -0,0 +1,69 @@
+package com.sorenpoulsen.ws;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.xml.ws.BindingProvider;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.tempuri.TestPortType;
+import org.tempuri.TestRequest;
+import org.tempuri.TestResponse;
+import org.tempuri.TestService;
+
+/**
+ * Test the JAX-WS service client by calling the SOAPUI mock service.
+ */
+public class ServiceTest {
+
+	String mockEndpoint;
+
+	@Before
+	public void readMockPort() {
+		InputStream is = this.getClass().getResourceAsStream("/test.properties");
+		java.util.Properties p = new Properties();
+		try {
+			p.load(is);
+		} catch (IOException e) {
+			Assert.fail();
+		}
+		String mockport = p.getProperty("mockport");
+		Assert.assertNotNull(mockport);
+		mockEndpoint = "http://localhost:" + mockport + "/testservice";
+	}
+
+	/**
+	 * Sending the message "hello" must return a "triggered" response.
+	 */
+	@Test
+	public void testTriggeredResponse() {
+		TestService testService = new TestService();
+		TestPortType testPort = testService.getTestPort();
+		BindingProvider binding = (BindingProvider) testPort;
+		binding.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, mockEndpoint);
+		TestRequest testRequest = new TestRequest();
+		testRequest.setMessage("hello");
+		TestResponse sendMessage = testPort.sendMessage(testRequest);
+		String response = sendMessage.getResponse();
+		Assert.assertEquals("triggered", response);
+	}
+
+	/**
+	 * Sending any other message must return the "Default" response
+	 */
+	@Test
+	public void testDefaultResponse() {
+		TestService testService = new TestService();
+		TestPortType testPort = testService.getTestPort();
+		BindingProvider binding = (BindingProvider) testPort;
+		binding.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, mockEndpoint);
+		TestRequest testRequest = new TestRequest();
+		testRequest.setMessage("xyz");
+		TestResponse sendMessage = testPort.sendMessage(testRequest);
+		String response = sendMessage.getResponse();
+		Assert.assertEquals("Default", response);
+	}
+}
